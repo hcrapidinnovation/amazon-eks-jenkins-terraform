7a8c7bf9605fce9badf7b9ded2bd989edbee1154
hcrapidinnovation <69822624+hcrapidinnovation@users.noreply.github.com>
2022-02-21 01:02:22 +0530

    Update install_jdk_maven.sh

--- terraform/install_jdk_maven.sh
+++ terraform/install_jdk_maven.sh
@@ -2,13 +2,10 @@
 
 #!/bin/bash
 sudo yum -y update
+sudo yum remove -y java
+sudo yum install -y java-1.8.0-openjdk
 
-echo "Install Java JDK 8"
-yum remove -y java
-yum install -y java-1.8.0-openjdk
-
-echo "Install Maven"
-yum install -y maven 
+sudo yum install -y maven 
 sudo git clone https://github.com/scmgalaxy/helloworld-java-maven.git
 cd helloworld-java-maven
-mvn clean install
+sudo mvn clean install
